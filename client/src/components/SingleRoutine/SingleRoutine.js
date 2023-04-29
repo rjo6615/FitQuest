872078df37f1831cf5ce3b8e3be56b9e9fd9d4a2
rjo6615 <robertjobernier@gmail.com>
2023-04-28 20:25:16 -0400

    got singleroutine page information to display

--- client/src/components/SingleRoutine/SingleRoutine.js
+++ client/src/components/SingleRoutine/SingleRoutine.js
@@ -3,23 +3,25 @@ import { useParams, useNavigate } from "react-router";
 // import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText} from 'mdb-react-ui-kit';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+// import Row from 'react-bootstrap/Row';
+// import Button from 'react-bootstrap/Button';
+// import Modal from 'react-bootstrap/Modal';
  
 export default function SingleRoutine() {
  const [form, setForm] = useState({
-   routines: "",
-   askAI: [{},{},{},{},{},{},{},{},{},{}],
+  routineName: "", 
+  age: "",
+  sex: "",
+  height: "",
+  currentWeight: "",
+  targetWeight: "",
+  goal: "",
+  workoutDifficulty: "",
+  calorieIntake: "",
+  calorieMaintain: "",
+  daysToTarget: "",
  });
  
- const [modalData, setModalData] = useState({
-  routines: "",
-  askAI: [{},{},{},{},{},{},{},{},{},{}],
- });
- const [show, setShow] = useState(false);
- const handleClose = () => setShow(false);
- const handleShow = () => setShow(true);
  const params = useParams();
  const navigate = useNavigate();
  
@@ -52,41 +54,19 @@ export default function SingleRoutine() {
  // This following section will display the workout data from the db.
  return (
   <center className="pb-4">
-     <div className="container">
-     <h1 className="text-light">{form.routines}</h1>
-     <h2 className="text-light">{form.askAI[0].type}</h2>
-     <Row xs={1} md={2} lg={3} xl={5} className="g-4 mx-4">  
-  {form.askAI.map((el) => (  
- <Col>          
- <Card style={{ maxWidth: 600, minHeight: 250, maxHeight: 250, backgroundColor: "lightGray"}}>
-   {/* <Card.Img className="mx-auto" style={{ marginBottom: '20px', marginTop: '15px', maxWidth: '16rem', borderRadius: '25px'}} variant="top" src={el.image} />               */}
+     <div className="">
+     <h1 className="text-light">{form.routineName}</h1>
+     <h2 className="text-light">Age: {form.age}</h2>
+ <Col className="mx-5">          
+ <Card>
    <Card.Body>
-     <Card.Title>{el.name}</Card.Title>
-     <Card.Subtitle className="mb-2 text-muted">Muscle: {el.muscle}</Card.Subtitle>
-     <Card.Subtitle className="mb-2 text-muted">Equipment: {el.equipment}</Card.Subtitle>
-     <Card.Subtitle className="mb-2 text-muted">Difficulty: {el.difficulty}</Card.Subtitle>
-     <Card.Text>  
-     {/* {el.instructions} */}
-     </Card.Text>
+     <Card.Title>Suggested Calorie Intake: {form.calorieIntake}</Card.Title>
+     <Card.Subtitle className="mb-2 text-muted">Days to Target Weight: {form.daysToTarget}</Card.Subtitle>
+     <Card.Subtitle className="mb-2 text-muted">Calories to Maintain Weight: {form.calorieMaintain}</Card.Subtitle>
+     <Card.Subtitle className="mb-2 text-muted">Target Weight: {form.targetWeight}</Card.Subtitle>
    </Card.Body>
-      <Button variant="secondary" onClick={() => {handleShow(); setModalData(el);}}>
-        View Instructions
-      </Button>      
  </Card>            
-</Col>
-  ))}
-  </Row>
-  <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>{modalData.name}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>{modalData.instructions}</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
+ </Col>
   </div>
 </center>
 );
