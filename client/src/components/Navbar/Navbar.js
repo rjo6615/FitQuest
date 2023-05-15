d4a7bacc4b60e3ce4706c02ea6a3b7758e775bb8
rjo6615 <robertjobernier@gmail.com>
2023-05-15 14:36:40 -0400

    changed background color of navbar toggle

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -14,7 +14,7 @@ function NavbarComponent() {
             <img src="./favicon.ico" className="img-fluid" alt="logo"></img>
         </div>
           <Navbar.Brand className="text-light" href="/">Fit Quest</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Toggle aria-controls="navbarScroll" className="text-light bg-light" />
           <Navbar.Collapse id="navbarScroll">
             <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
               <Nav.Link className="text-light" href="/create-routine">Create Routine</Nav.Link>
